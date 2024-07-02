77b396f8ef9058d721b3d0406519dea6f9b95bb3
bishboy <bishboy2spieking@gmail.com>
2024-07-02 11:16:07 +0100

    best beaches in the world

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "../component/Main";
+import About from "../component/About";
+import Discover from "../component/Discover";
+import Eperience from "../component/Eperience";
+import Sroll from "../component/Sroll";
+
+const Home = () => {
+  return (
+    <div>
+        <Main   />
+        <About  />
+        <Discover  />
+        <Eperience  />
+    </div>
+  );
+};
+
+export default Home;
